b72ef75893192bcfaf2cf7fa2e976e133caa3f8e
Aditya Pande <adityapande@google.com>
2024-01-02 17:51:37 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="linux_isolate_process",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[],
 )
