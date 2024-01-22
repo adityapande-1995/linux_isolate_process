52d2cd7a00a90ae2b7df9d1f652bf05bfe4a4dcf
Aditya Pande <adityapande@google.com>
2024-01-22 23:49:46 +0000

    Changed to a ROS package
    Signed-off-by: Aditya Pande <adityapande@google.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+package_name = 'linux_isolate_process'
 
 setup(
-    name="linux_isolate_process",
-    version="0.1.3",
-    packages=find_packages(),
-    install_requires=[],
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='adityapande',
+    maintainer_email='adityapande@intrinsic.ai',
+    description='A package isolate ROS2 nodes',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
 )
