d1905db6785453e2a710296b2c83bc7e3e077b3e
Aditya Pande <adityapande@google.com>
2024-01-23 19:32:52 +0000

    0.0.1
    Signed-off-by: Aditya Pande <adityapande@google.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'linux_isolate_process'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
